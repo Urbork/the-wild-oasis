6acbbd4e2606c7dbd9e4cead079e352924159e23
Jonasz Jachym <jajonaszjj@gmail.com>
2025-03-29 08:43:15 +0100

    styled components started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: lavenderblush;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => console.log("check in")}>Check in</Button>
+        <Button onClick={() => console.log("check out")}>Check out</Button>
+
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guestes" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
