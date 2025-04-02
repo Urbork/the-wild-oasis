fca95053b594dacecb3637a2338916a5f415febd
Jonasz Jachym <jajonaszjj@gmail.com>
2025-04-02 17:56:29 +0200

    Cabins and delete button added

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,23 @@
-import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import Heading from "../ui/Heading";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
+  // useEffect(function () {
+  //   getCabins().then((data) => console.log(data));
+  // }, []);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+        {/* <img src="https://jhbvfaqiwoxzlmohsbui.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg" /> */}
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
