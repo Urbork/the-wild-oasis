74eb5abed53622f5251fa1b6405c9806190b88ea
Jonasz Jachym <jajonaszjj@gmail.com>
2025-04-07 11:44:04 +0200

    supabaseUrl exported

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jhbvfaqiwoxzlmohsbui.supabase.co";
+export const supabaseUrl = "https://jhbvfaqiwoxzlmohsbui.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoYnZmYXFpd294emxtb2hzYnVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNDg2OTMsImV4cCI6MjA1ODgyNDY5M30.oqw9M_uxkNQcVOkZN16F3NtcHjJz9AafRuvlFduwqso";
 const supabase = createClient(supabaseUrl, supabaseKey);
