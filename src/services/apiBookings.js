41691139bf2406b946e9910218dfaed04ca5a2d6
Jonasz Jachym <jajonaszjj@gmail.com>
2025-04-27 08:05:25 +0200

    sorting bookings added

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -12,8 +12,15 @@ export async function getBookings({ filter, sortBy }) {
   // .gte("totalPrice", 5000);
 
   // FILTER
-  if (filter !== null) {
+  if (filter) {
     query = query[filter.method || "eq"](filter.field, filter.value);
+  }
+
+  // SORT
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
   }
 
   const { data, error } = await query;
