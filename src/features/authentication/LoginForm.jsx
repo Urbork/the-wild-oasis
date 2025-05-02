57af2927d417773f7f13f1c8e22dd0f17d1b410d
Jonasz Jachym <jajonaszjj@gmail.com>
2025-05-02 07:30:07 +0200

    authentication protecting routes

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,7 +7,7 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("jonas@example.com ");
+  const [email, setEmail] = useState("jonas@example.com");
   const [password, setPassword] = useState("pass0987");
   const { login, isLoading } = useLogin();
 
@@ -16,7 +16,15 @@ function LoginForm() {
 
     if (!email || !password) return;
 
-    login({ email: email.trim(), password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
