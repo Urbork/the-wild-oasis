c6598e5b2453b41ce7ced5c91d58b00f00778a4f
Jonasz Jachym <jajonaszjj@gmail.com>
2025-05-02 07:48:16 +0200

    user logout and bug fix added

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,8 +11,11 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      console.log(user);
+
+      // queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (error) => {
