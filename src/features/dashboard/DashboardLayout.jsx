4e8e258f9da1f68e9550e27bde0beb0651a50741
Jonasz Jachym <jajonaszjj@gmail.com>
2025-05-06 10:42:01 +0200

    working on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,15 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Todays</div>
+      <div>Chart</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
