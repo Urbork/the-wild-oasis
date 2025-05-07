70e465ce4431ce7df3249bf8990d078dd4a79f24
Jonasz Jachym <jajonaszjj@gmail.com>
2025-05-07 10:04:06 +0200

    recent bookings and stays added

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -59,11 +59,11 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
-export async function getBookingsAfterDate(date) {
+export async function getBookingsAfterDate(dateISO) {
   const { data, error } = await supabase
     .from("bookings")
     .select("created_at, totalPrice, extrasPrice")
-    .gte("created_at", date)
+    .gte("created_at", dateISO)
     .lte("created_at", getToday({ end: true }));
 
   if (error) {
@@ -75,12 +75,12 @@ export async function getBookingsAfterDate(date) {
 }
 
 // Returns all STAYS that are were created after the given date
-export async function getStaysAfterDate(date) {
+export async function getStaysAfterDate(dateISO) {
   const { data, error } = await supabase
     .from("bookings")
     // .select('*')
     .select("*, guests(fullName)")
-    .gte("startDate", date)
+    .gte("startDate", dateISO)
     .lte("startDate", getToday());
 
   if (error) {
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -20,6 +20,7 @@ export const getToday = function (options = {}) {
   if (options?.end)
     // Set to the last second of the day
     today.setUTCHours(23, 59, 59, 999);
+  // Set to the first second of the day
   else today.setUTCHours(0, 0, 0, 0);
   return today.toISOString();
 };
